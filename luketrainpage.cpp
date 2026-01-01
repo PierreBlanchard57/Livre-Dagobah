95754c3535eee9d744c30fccd9c510e738b13e16
Wire0n <pierre1239@hotmail.fr>
2026-01-01 13:25:15 +0100

    events page entrainement luke

--- luketrainpage.cpp
+++ luketrainpage.cpp
@@ -1,7 +1,42 @@
 #include "luketrainpage.h"
 #include "page.h"
+#include <QMouseEvent>
+#include <QEvent>
+#include <QDebug>
 LukeTrainPage::LukeTrainPage(QWidget *parent,MainWindow *mainwindow) : Page(parent,mainwindow),ui(new Ui::LukeTrainPage)
 {
 ui->setupUi(this);
+ui->rock1->installEventFilter(this);
+ui->rock2->installEventFilter(this);
+ui->rock3->installEventFilter(this);
 }
 
+bool LukeTrainPage::eventFilter(QObject *watched, QEvent *event){
+
+if(watched== ui->rock1 || watched==ui->rock2 || watched==ui->rock3){
+    if (event->type() == QEvent::MouseButtonPress) {
+                QMouseEvent *mouseEvent = static_cast<QMouseEvent *>(event);
+                if (mouseEvent->button() == Qt::LeftButton) {
+                        qDebug() << "enfoncé";
+                        changeLukePose(":/luke_force.png");
+                }
+            }
+    if (event->type() == QEvent::MouseButtonRelease) {
+                QMouseEvent *mouseEvent = static_cast<QMouseEvent *>(event);
+                if (mouseEvent->button() == Qt::LeftButton) {
+                    changeLukePose(":/luke_normal.png");
+                        qDebug() << "relaché";
+                }
+            }
+    if (event->type() == QEvent::MouseMove) {
+                        qDebug() << "souris bougé!";
+            }
+}
+return false;
+}
+
+void LukeTrainPage::changeLukePose(const std::string &file)
+{
+
+    ui->luke->setPixmap(QPixmap(QString::fromStdString(file)));
+}
--- luketrainpage.h
+++ luketrainpage.h
@@ -2,6 +2,8 @@
 #define LUKETRAINPAGE_H
 
 #include <QWidget>
+#include <QObject>
+#include <QEvent>
 #include "page.h"
 #include "ui_luketrainpage.h"
 class LukeTrainPage : public Page
@@ -13,6 +15,9 @@ private:
     Ui::LukeTrainPage *ui;
     int rocksPlaced=0;
     bool isDragging=false;
+    const double gravity=5;
+    bool eventFilter(QObject *watched, QEvent *event);
+    void changeLukePose(const std::string &file);
     void unlock();
 };
 
