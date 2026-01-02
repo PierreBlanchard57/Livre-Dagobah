2de4f846be69769e066dd82292883ae8d52a0e25
vitawrap <thevitawrap@gmail.com>
2026-01-02 23:18:06 +0100

    Menu: Page titre et musique

--- titlepage.cpp
+++ titlepage.cpp
@@ -3,6 +3,20 @@
 
 TitlePage::TitlePage(QWidget *parent) : Page(parent,nullptr),ui(new Ui::TitlePage)
 {
- ui->setupUi(this);
- Page::setPageFinished();//C'est la page de titre alors elle est directement terminée
+    ui->setupUi(this);
+    Page::setPageFinished();//C'est la page de titre alors elle est directement terminée
+
+    audioList.addMedia(QUrl("./pages_m/menu.wav"));
+    audioList.setPlaybackMode(QMediaPlaylist::Loop);
+    musicPlayer.setPlaylist(&audioList);
+}
+
+void TitlePage::showEvent(QShowEvent *event)
+{
+    musicPlayer.play();
+}
+
+void TitlePage::hideEvent(QHideEvent *event)
+{
+    musicPlayer.stop();
 }
--- titlepage.h
+++ titlepage.h
@@ -1,5 +1,7 @@
 
 #include <QWidget>
+#include <QMediaPlayer>
+#include <QMediaPlaylist>
 #include "page.h"
 #include "ui_titlepage.h"
 
@@ -7,6 +9,14 @@ class TitlePage : public Page {
     Q_OBJECT
 public:
     explicit TitlePage(QWidget *parent = nullptr);
+
+protected:
+    void showEvent(QShowEvent *event) override;
+    void hideEvent(QHideEvent *event) override;
+
 private:
     Ui::TitlePage *ui;
+
+    QMediaPlayer musicPlayer;
+    QMediaPlaylist audioList;
 };
